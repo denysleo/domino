f01a5eb2ee5b8bc456aa617b9336483982c33c87
teteu <dinizteteu456@gmail.com>
2025-05-21 09:44:35 -0300

    Corrige o menu na interface

--- src/menu.c
+++ src/menu.c
@@ -7,33 +7,40 @@
 int menuInicial() {
     int opcao = 1;
     int tecla = 0;
+    int mudou = 1; // flag para redesenhar tela
 
     while (tecla != 10) {  // 10 = ENTER
-        screenClear();
-        screenSetColor(WHITE, BLACK);
-        printf("===== MENU =====\n\n");
+        if (mudou) {
+            screenClear();
+            screenSetColor(WHITE, BLACK);
+            printf("===== MENU =====\n\n");
 
-        for (int i = 1; i <= 3; i++) {
-            if (i == opcao)
-                screenSetColor(YELLOW, BLACK);
-            else
-                screenSetColor(WHITE, BLACK);
+            for (int i = 1; i <= 3; i++) {
+                if (i == opcao)
+                    screenSetColor(YELLOW, BLACK);
+                else
+                    screenSetColor(WHITE, BLACK);
 
-            if (i == 1)
-                printf("%d. Iniciar Jogo\n", i);
-            else
-                printf("%d. Opção %d\n", i, i);
+                if (i == 1)
+                    printf("%d. Iniciar Jogo\n", i);
+                else
+                    printf("%d. Opção %d\n", i, i);
+            }
+
+            screenUpdate();
+            mudou = 0; // tela atualizada, espera mudança
         }
-
-        screenUpdate();
 
         if (keyhit()) {
             tecla = readch();
 
-            if (tecla == 65 && opcao > 1)
+            if (tecla == 65 && opcao > 1) {
                 opcao--;
-            else if (tecla == 66 && opcao < 3)
+                mudou = 1; // mudou seleção, redesenha
+            } else if (tecla == 66 && opcao < 3) {
                 opcao++;
+                mudou = 1; // mudou seleção, redesenha
+            }
         }
     }
 
